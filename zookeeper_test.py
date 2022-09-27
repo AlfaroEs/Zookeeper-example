b2d26010abfa83ddcf78646117020e2d994ec97d
AlfaroEs <sabua2000@gmail.com>
2022-09-27 15:59:05 -0500

    add tester

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 import unittest
 from zookeeper import Ztree
 
@@ -12,6 +13,37 @@ class TestZookeeper(unittest.TestCase):
         with self.assertRaises(Exception):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
+    
+    def test_cambio_data(self):
+        tree = Ztree()
+        tree.create('/node1','data vieja',False,True,15,'/')
+        tree.setData('/node1','nueva data')
+        self.assertEqual(tree.getData('/node1'),'nueva data')
+    
+    def test_busqueda_nodo(self):
+        tree = Ztree()
+        tree.create('/node1','data',False,True,15,'/')
+        self.assertFalse(exists('/node2'))
+    
+    def test_eliminacion_nodo_regular(self):
+        tree = Ztree()
+        tree.create('/node1','data',False,True,15,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(exists('/node1'))
+
+    def test_eliminacion_nodo_ephemero(self):
+        tree = Ztree()
+        tree.create('/node1','data',True,True,15,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(exists('/node1'))
+    
+    def test_eliminacion_nodo_ephemero_inactivo(self):
+        tree = Ztree()
+        tree.create('/node1','data',True,False,15,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(exists('/node1'))
+        
+        
 
 
 if __name__ == '__main__':
